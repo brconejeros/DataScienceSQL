178950f5da265269103169b9369b2ff89aa703e1
Bruno Conejeros <53882404+brconejeros@users.noreply.github.com>
2023-12-12 17:35:17 -0300

    Update Exercicio8-Cap10.sql

--- Cap10/Exercicio8-Cap10.sql
+++ Cap10/Exercicio8-Cap10.sql
@@ -27,23 +27,76 @@ INSERT INTO cap10.vendas (ID, DataVenda, Produto, Quantidade, ValorUnitario, Ven
 
 
 -- Pergunta 1: Qual o total de vendas por produto?
-
+select 
+	produto,
+	sum(quantidade*valorunitario) total_vendas
+from cap10.vendas
+group by 1
+order by 2 desc;
 
 -- Pergunta 2: Qual o total de vendas por vendedor?
-
+select 
+	vendedor,
+	sum(quantidade*valorunitario) total_vendas
+from cap10.vendas
+group by 1
+order by 2 desc;
 
 -- Pergunta 3: Qual o total de vendas por dia?
-
+select 
+	datavenda,
+	sum(quantidade*valorunitario) total_vendas
+from cap10.vendas
+group by 1
+order by 1 desc;
 
 -- Pergunta 4: Como as vendas se acumulam por dia e por produto (incluindo subtotais diários)?
+select 
+	coalesce(to_char(datavenda, 'yyyy-mm-dd'), 'Total por Dia') data_venda,
+	coalesce(produto, 'Total por Produto') produto,
+	sum(quantidade*valorunitario) total_vendas
+from cap10.vendas
+group by 
+	rollup(datavenda, produto)
+order by 
+	1, 2;
 
+select 
+	coalesce(to_char(datavenda, 'yyyy-mm-dd'), 'Total por Dia') data_venda,
+	coalesce(produto, 'Total por Produto') produto,
+	sum(quantidade*valorunitario) total_vendas
+from cap10.vendas
+group by 
+	rollup(datavenda, produto)
+order by 
+	grouping(datavenda, produto), 1, 2;
 
 -- Pergunta 5: Qual a combinação de vendedor e produto gerou mais vendas (incluindo todos os subtotais possíveis)?
-
+select 
+	coalesce(vendedor, 'Total por Vendedor') vendedor,
+	coalesce(produto, 'Total por Produto') produto,
+	sum(quantidade*valorunitario) total_vendas
+from cap10.vendas
+group by 
+	cube(vendedor, produto)
+order by 
+	grouping(vendedor, produto), total_vendas desc;
 
 -- Imagine que você queira analisar as vendas totais por Produto, por Vendedor e também o total geral de todas as vendas. 
 -- Como seria a Query SQL?
-
+select 
+	coalesce(vendedor, 'Todos') vendedor,
+	coalesce(produto, 'Todos') produto,
+	sum(quantidade*valorunitario) total_vendas
+from cap10.vendas
+group by grouping sets (
+	(produto),
+	(vendedor),
+	()
+)
+order by 
+	grouping(vendedor, produto), total_vendas desc
+;
 
 
 
